08af4f4b9f7e456c637642921f59bb1ebf7eacf7
aarchadeloitte <152913451+aarchadeloitte@users.noreply.github.com>
2024-01-11 12:03:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ var getScriptPromisify = (src) => {
 			  display: none; /* Hide tooltip by default */
 			}
         </style>
-		<svg id="map" width="800" height="800"></svg>
+		<svg id="map" width="800" height="800" onclick="alert('You have clicked the circle.')" ></svg>
       `
   class Main extends HTMLElement {
     constructor () {
