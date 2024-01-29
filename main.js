0f09c07ed2161d554cd856957aedc9dccff30999
aarchadeloitte <152913451+aarchadeloitte@users.noreply.github.com>
2024-01-29 17:08:17 +0100

    1

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ var getScriptPromisify = (src) => {
 		<script id="GlData">
 		var Gdata = [],
 		</script>
-		<svg id="map" viewBox="0 0 600 600" preserveAspectRatio="xMinYMin"></svg>
+		<svg id="map" viewBox="0 0 500 500" preserveAspectRatio="xMinYMin"></svg>
       `
   class Main extends HTMLElement {
     constructor () {
@@ -103,7 +103,7 @@ var getScriptPromisify = (src) => {
 	//d3.json("https://raw.githubusercontent.com/isellsoap/deutschlandGeoJSON/main/2_bundeslaender/3_mittel.geo.json")
             .then(data => {
                 // Create a projection to transform geographic coordinates to SVG coordinates
-				const projection = d3.geoIdentity().fitSize([150,150], data);			//
+				const projection = d3.geoIdentity().fitSize([500,500], data);			//
                 // Create a path generator
                 const pathGenerator = d3.geoPath().projection(projection);
 				// Draw paths for each feature
