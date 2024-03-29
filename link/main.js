4f18eba4817cc31f3371549bb9f8789a4b24b661
aarchadeloitte <152913451+aarchadeloitte@users.noreply.github.com>
2024-03-29 17:06:28 +0100

    1

--- link/main.js
+++ link/main.js
@@ -6,7 +6,7 @@
           </style>
           
           <div id="root" style="width: 100%; height: 100%;">
-            <p><a id = "link_href" href="https://www.google.com/">Google</a></p>
+            <p><a id = "link_href" href="https://www.google.com/" target="_blank" >Google</a></p>
           </div>
         `
 
