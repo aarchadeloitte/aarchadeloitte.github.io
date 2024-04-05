91db22089e981eb55fc95f3814facfceb77d57b7
aarchadeloitte <152913451+aarchadeloitte@users.noreply.github.com>
2024-04-05 19:07:11 +0200

    1

--- postRequest/main.js
+++ postRequest/main.js
@@ -94,7 +94,7 @@
             //'Access-Control-Allow-Origin': 'https://itsvac-test.eu20.hcs.cloud.sap',
             //'Access-Control-Allow-Credentials': 'true',
             //'Cache-Control': 'no-cache',
-            //'Access-Control-Allow-Headers': 'X-Csrf-Token, x-csrf-token, x-sap-cid, Content-Type, Authorization, mysapsso2',
+            'Access-Control-Allow-Headers': 'X-Csrf-Token, x-csrf-token, x-sap-cid, Content-Type, Authorization, mysapsso2'
             //'Access-Control-Allow-Methods': 'GET, POST, PUT, OPTIONS',
             //'Access-Control-Expose-Headers': 'X-CSRF-TOKEN,SAP-REWRITEURL,SAP-URL-SESSION-ID,SAP-PERF-FESREC,SAP-SYSTEM',
             //'Access-Control-Max-Age': '60'
