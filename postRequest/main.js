4430760963236418422bd5fb7c9f3f6c7c0c947d
aarchadeloitte <152913451+aarchadeloitte@users.noreply.github.com>
2024-11-06 18:14:02 +0100

    1

--- postRequest/main.js
+++ postRequest/main.js
@@ -6,7 +6,7 @@
         </style>
         
         <div id="root" style="width: 100%; height: 100%;">
-          <p><a id = "link_href" href="https://www.google.com/" target="_blank" >Google</a></p>
+          <p><a id = "link_href" href="https://www.google.com/" target="_blank" >Post request</a></p>
         </div>
       `
 
